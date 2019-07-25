3a6408ec5e5b3966d5b3a43991b1651282104b7a
linuxwan <chungwan.park@gmail.com>
2019-07-25 22:18:36 +0900

    프로젝트 추가

---
+++ WANIGridTest/WANIGridTest.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WANIGridTest
+{
+    partial class WANIGridTest
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            WANI_Grid.Grid.Grid grid1 = new WANI_Grid.Grid.Grid();
+            this.waniGrid = new WANI_Grid.WANIGrid();
+            this.SuspendLayout();
+            // 
+            // waniGrid
+            // 
+            this.waniGrid.DataSource = null;
+            this.waniGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            grid1.FirstVisibleCol = 0;
+            grid1.FirstVisibleRow = 0;
+            grid1.GridDisplayType = WANI_Grid.Grid.GridType.DefaultType;
+            grid1.HeaderGen = null;
+            grid1.LastVisibleCol = 0;
+            grid1.LastVisibleRow = 0;
+            grid1.LeftHeaderWidth = 22;
+            grid1.TopHeaderHeight = 20;
+            this.waniGrid.Grid = grid1;
+            this.waniGrid.GridDisplayType = WANI_Grid.Grid.GridType.DefaultType;
+            this.waniGrid.HeaderFont = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold);
+            this.waniGrid.LeftHeaderWidth = 22;
+            this.waniGrid.Location = new System.Drawing.Point(0, 0);
+            this.waniGrid.Name = "waniGrid";
+            this.waniGrid.Size = new System.Drawing.Size(613, 372);
+            this.waniGrid.TabIndex = 0;
+            // 
+            // WANIGridTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 372);
+            this.Controls.Add(this.waniGrid);
+            this.Name = "WANIGridTest";
+            this.Text = "WANI Grid Test";
+            this.Load += new System.EventHandler(this.WANIGridTest_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private WANI_Grid.WANIGrid waniGrid;
+    }
+}
+
---
+++ WANIGridTest/WANIGridTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WANI_Grid.Grid.Head;
+
+namespace WANIGridTest
+{
+    public partial class WANIGridTest : Form
+    {
+        public WANIGridTest()
+        {
+            InitializeComponent();
+        }
+
+        private void WANIGridTest_Load(object sender, EventArgs e)
+        {
+            HeaderBuilder builder = new HeaderBuilder(this.waniGrid.GridDisplayType);
+            builder.AddHeader(new DefaultHeader("Col01", "Column 01", 80, HorizontalAlignment.Center, HorizontalAlignment.Left, true));
+            builder.AddHeader(new DefaultHeader("Col02", "Column 02", 80, HorizontalAlignment.Center, HorizontalAlignment.Left, true));
+            builder.AddHeader(new DefaultHeader("Col03", "Column 03", 90, HorizontalAlignment.Center, HorizontalAlignment.Left, true));
+            builder.AddHeader(new DefaultHeader("Col04", "Column 04", 100, HorizontalAlignment.Center, HorizontalAlignment.Left, true));
+            builder.AddHeader(new DefaultHeader("Col05", "Column 05", 110, HorizontalAlignment.Center, HorizontalAlignment.Left, true));
+            builder.AddHeader(new DefaultHeader("Col06", "Column 06", 120, HorizontalAlignment.Center, HorizontalAlignment.Left, true));
+            builder.AddHeader(new DefaultHeader("Col07", "Column 07", 110, HorizontalAlignment.Center, HorizontalAlignment.Left, true));
+            builder.AddHeader(new DefaultHeader("Col08", "Column 08", 100, HorizontalAlignment.Center, HorizontalAlignment.Left, true));
+            builder.AddHeader(new DefaultHeader("Col09", "Column 09", 80, HorizontalAlignment.Center, HorizontalAlignment.Left, true));
+            builder.AddHeader(new DefaultHeader("Col10", "Column 10", 90, HorizontalAlignment.Center, HorizontalAlignment.Left, true));
+            builder.InitializeHeader();
+            this.waniGrid.SetHeader(builder);
+            //this.waniGrid.SelectedColor = Color.LightBlue;
+        }
+    }
+}
